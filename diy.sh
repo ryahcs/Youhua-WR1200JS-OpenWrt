5dce75fcb6575275a6194b4ff83baf22b5a1ed8c
ryahcs <ryahcs@gmail.com>
2020-04-19 21:46:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,10 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#
+#添加SSR PLUS+
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
