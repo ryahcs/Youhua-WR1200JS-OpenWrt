503ff526e34f81fd41805a8960b2c5231ef343d8
ryahcs <ryahcs@gmail.com>
2020-04-19 21:47:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,5 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 #
 #添加SSR PLUS+
-mkdir package/base-files/files/config
-echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
